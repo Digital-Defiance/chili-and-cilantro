f82cc14d8856768c3cd3ee54d6afcc85d5a23f0e
Jessica Mulein <jessica@digitaldefiance.org>
2023-11-29 20:33:17 +0000

    add more tests to gameService

--- chili-and-cilantro-api/test/unit/gameService.test.ts
+++ chili-and-cilantro-api/test/unit/gameService.test.ts
@@ -6,22 +6,21 @@ import { generateGame } from '../fixtures/game';
 import mongoose from 'mongoose';
 
 describe('GameService', () => {
+  let gameService;
+  let mockActionService;
+  let mockChefService;
+  let mockPlayerService;
+  let mockGameModel;
+
+  beforeEach(() => {
+    const database = new Database();
+    mockGameModel = database.getModel<IGame>(ModelName.Game);
+    mockActionService = {};
+    mockChefService = {};
+    mockPlayerService = {};
+    gameService = new GameService(mockGameModel, mockActionService, mockChefService, mockPlayerService);
+  });
   describe("getGameByIdOrThrowAsync", () => {
-    let gameService;
-    let mockActionService;
-    let mockChefService;
-    let mockPlayerService;
-    let mockGameModel;
-
-    beforeEach(() => {
-      const database = new Database();
-      mockGameModel = database.getModel<IGame>(ModelName.Game);
-      mockActionService = {};
-      mockChefService = {};
-      mockPlayerService = {};
-      gameService = new GameService(mockGameModel, mockActionService, mockChefService, mockPlayerService);
-    });
-
     it('should return a game when found', async () => {
       const mockGame = generateGame(true);
       jest.spyOn(mockGameModel, 'findOne').mockResolvedValue(mockGame);
@@ -50,6 +49,58 @@ describe('GameService', () => {
 
       expect(mockGameModel.findOne).toHaveBeenCalledWith({
         _id: new mongoose.Types.ObjectId(gameId),
+        currentPhase: { $ne: GamePhase.GAME_OVER },
+      });
+    });
+  });
+  describe('getGameByCodeOrThrowAsync', () => {
+    it('should return the most recent game when found by code', async () => {
+      const mockGame = generateGame(true);
+      const findMock = jest.fn().mockReturnValue({
+        sort: jest.fn().mockReturnValue({
+          limit: jest.fn().mockReturnValue({
+            then: jest.fn().mockResolvedValue(mockGame),
+          }),
+        }),
+      });
+      jest.spyOn(mockGameModel, 'find').mockImplementation(findMock);
+
+      const gameCode = 'testCode';
+      const result = await gameService.getGameByCodeOrThrowAsync(gameCode);
+
+      expect(result).toBe(mockGame);
+      expect(mockGameModel.find).toHaveBeenCalledWith({ code: gameCode });
+    });
+    it('should throw InvalidGameError when no game is found', async () => {
+      const findMock = jest.fn().mockReturnValue({
+        sort: jest.fn().mockReturnValue({
+          limit: jest.fn().mockReturnValue({
+            then: jest.fn().mockResolvedValue(null),
+          }),
+        }),
+      });
+      jest.spyOn(mockGameModel, 'find').mockImplementation(findMock);
+
+      const gameCode = 'testCode';
+
+      await expect(gameService.getGameByCodeOrThrowAsync(gameCode)).rejects.toThrow(InvalidGameError);
+    });
+    it('should search for active games when active parameter is true', async () => {
+      const mockGame = generateGame(true);
+      const findMock = jest.fn().mockReturnValue({
+        sort: jest.fn().mockReturnValue({
+          limit: jest.fn().mockReturnValue({
+            then: jest.fn().mockResolvedValue(mockGame),
+          }),
+        }),
+      });
+      jest.spyOn(mockGameModel, 'find').mockImplementation(findMock);
+
+      const gameCode = 'testCode';
+      await gameService.getGameByCodeOrThrowAsync(gameCode, true);
+
+      expect(mockGameModel.find).toHaveBeenCalledWith({
+        code: gameCode,
         currentPhase: { $ne: GamePhase.GAME_OVER },
       });
     });
